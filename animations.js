76c544d2fb2464bad02e9f4220a59b1d993a3805
YasserDalali <yasser.dalali.personal@gmail.com>
2024-04-04 18:15:26 +0200

    added new section

--- animations.js
+++ animations.js
@@ -10,7 +10,7 @@
         }
 
         requestAnimationFrame(raf);
-
+/* ========================== */
 
         function getImageFileName() {
             var imageItem = document.getElementById('imageItem');
@@ -21,7 +21,73 @@
         
         /* ------------------------------------------- */
         window.addEventListener('load', function () {
-            gsap.from('.animated-title', {opacity: 0, duration: 1, y: -50 });
+            // GSAP Animation
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animated-title', {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    scrollTrigger: {
+        trigger: '.animated-title',
+        start: 'top 80%',
+        end: 'top 50%',
+        toggleActions: 'play none none reverse'
+    }
+});
+
+gsap.from('.subtitle', {
+    opacity: 0,
+    y: -20,
+    duration: 1,
+    scrollTrigger: {
+        trigger: '.animated-title',
+        start: 'top 80%',
+        end: 'top 50%',
+        toggleActions: 'play none none reverse'
+    }
+});
+
+gsap.from('.left-col', {
+    opacity: 0,
+    x: -50,
+    duration: 1,
+    scrollTrigger: {
+        trigger: '.left-col',
+        start: 'top 80%',
+        end: 'top 50%',
+        toggleActions: 'play none none reverse'
+    }
+
+    
+});
+
+gsap.from('.right-col', {
+    opacity: 0,
+    x: 50,
+    duration: 1,
+    scrollTrigger: {
+        trigger: '.right-col',
+        start: 'top 80%',
+        end: 'top 50%',
+        toggleActions: 'play none none reverse'
+    }
+});
+
+        });
+        gsap.to('.circle', {
+            opacity: 1,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: '.circle',
+                start: 'top top', // when the top of the circle reaches the top of the viewport
+                end: 'bottom bottom', // when the bottom of the circle reaches the bottom of the viewport
+                pin: true,
+                onUpdate: self => {
+                    const circle = document.querySelector('.circle');
+                    circle.style.top = `${self.scrollY}px`;
+                }
+            }
         });
         
         function switchItem(link) {
@@ -30,13 +96,16 @@
             var imageItem = document.getElementById('imageItem');
             if (getImageFileName() != link) {
 
-                gsap.to(imageItem, {opacity:0, duration: 0.5, x: 900, })
+                gsap.to(imageItem, {opacity:0, duration: 0.2, x: 900, })
 
                 gsap.to(imageItem, {opacity:1, duration: 0.5, x:0, onStart: function() {
-                    imageItem.src = link;}, delay:0.1})
+                    imageItem.src = link;}, delay:0.04})
     
     
             }
            
 
         }
+
+        /*!=========== ======================================= */
+
