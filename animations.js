0ea2253de79092612e18625194c64d4ad4fe8b28
YasserDalali <yasser.dalali.personal@gmail.com>
2024-04-04 17:37:05 +0200

    created a functionning image switcher with animation

--- animations.js
+++ animations.js
@@ -1,7 +1,4 @@
- 
-        gsap.from('.animated-title', { opacity: 0, duration: 1, y: -50, ease: 'easeInOut' });
-
-        const lenis = new Lenis();
+       const lenis = new Lenis();
 
         lenis.on('scroll', (e) => {
             console.log(e);
@@ -13,3 +10,33 @@
         }
 
         requestAnimationFrame(raf);
+
+
+        function getImageFileName() {
+            var imageItem = document.getElementById('imageItem');
+            var src = imageItem.src;
+            var fileName = src.substring(src.lastIndexOf('/') + 1);
+            return fileName;
+        }
+        
+        /* ------------------------------------------- */
+        window.addEventListener('load', function () {
+            gsap.from('.animated-title', {opacity: 0, duration: 1, y: -50 });
+        });
+        
+        function switchItem(link) {
+
+
+            var imageItem = document.getElementById('imageItem');
+            if (getImageFileName() != link) {
+
+                gsap.to(imageItem, {opacity:0, duration: 0.5, x: 900, })
+
+                gsap.to(imageItem, {opacity:1, duration: 0.5, x:0, onStart: function() {
+                    imageItem.src = link;}, delay:0.1})
+    
+    
+            }
+           
+
+        }
